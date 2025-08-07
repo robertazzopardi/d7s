46bab30896b2e01bda1c45677b95aaecec40ee8f
Rob <robertazzopardi@users.noreply.github.com>
2025-08-08 00:11:07 +0100

    Updated connections table, navigation and displaying saved connections

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use app::App;
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
-    let result = App::new().run(terminal).await;
+    let result = App::default().run(terminal).await;
     ratatui::restore();
     result
 }
